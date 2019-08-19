efeceaa212e4f93e5b3631e278a7c9208157e883
Thomas Huber <huber.thomas@outlook.com>
2019-08-19 18:11:33 +0200

    added comment

--- edgelib/Canny.py
+++ edgelib/Canny.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-
+'''
+https://towardsdatascience.com/canny-edge-detection-step-by-step-in-python-computer-vision-b49c3a2d8123
+https://github.com/FienSoP/canny_edge_detector
+'''
 def canny(img: np.ndarray = None,
           threshold1: int = 100,
           threshold2: int = 150,
