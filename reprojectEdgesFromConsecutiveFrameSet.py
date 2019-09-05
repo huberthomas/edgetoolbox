09f21a79e784058fb6052c74dae8e8fd75caabc7
Thomas Huber <huber.thomas@outlook.com>
2019-09-05 14:49:26 +0200

    changed input / output

--- reprojectEdgesFromConsecutiveFrameSet.py
+++ reprojectEdgesFromConsecutiveFrameSet.py
@@ -117,7 +117,7 @@ def parseArgs() -> any:
                         default=1, help='Set the frame projection mode. 1 is backprojection, 2 is reprojection and 3 is center frame projection. Default is 1.')
     parser.add_argument('-id', '--inpaintDepth', type=int, choices=[0, 1, 2], default=0,
                         help='Fill out undefined depth regions by inpainting. 0 ... off, 1 ... CV_INPAINT_NS, 2 ... CV_INPAINT_TELEA. Default is 0.')
-    parser.add_argument('-s', '--scales', type=int, choices=[0,1,2], default=0, help='Set the scale level. 0 = 1:1, 1 = 1:2, 2 = 1:4')
+    parser.add_argument('-s', '--scales', type=int, choices=[0, 1, 2], default=0, help='Set the scale level. 0 = 1:1, 1 = 1:2, 2 = 1:4')
     parser.add_argument('-mipa', '--minIsolatedPixelArea', type=int, default=0, help='Minimum isolated pixel areas. Cleans contours less or equal this value.')
 
     return parser.parse_args()
@@ -130,84 +130,135 @@ def main() -> None:
     try:
         args = parseArgs()
         args = checkInputParameter(args)
-        print(Utilities.argsToStr(args))
 
-        # write configuration to output directory
-        f = open(os.path.join(args.outputDir, 'settings.txt'), 'w')
-        f.write(Utilities.argsToStr(args))
-        f.close()
+        # python reprojectEdgesFromConsecutiveFrameSet.py  -c  -o  -p 3 -f 3 -l 2 -u 2 -mipa 30
 
-        startTime = time.time()
-        logging.info('Loading data from camera calibration file.')
-        camera = Camera()
-        camera.loadFromFile(args.camCalibFile)
+        allBase = '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/all'
+        trainRgbBase = '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/mix'
+        datasetBase = '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets'
 
-        logging.info('Loading data from associated ground truth file.')
-        gtHandler = TumGroundTruthHandler()
-        gtHandler.load(args.groundTruthFile)
+        subDir = [
+            'rgbd_dataset_freiburg3_long_office_household'
+        ]
 
-        if len(gtHandler.data()) < args.frameOffset:
-            raise ValueError('Number of input data is "%s" but must be greater than the frame offset "%d".' % (len(gtHandler.data()), args.frameOffset))
+        for i in range(0, len(subDir)):
+            args.rgbDir = os.path.join(trainRgbBase, 'rgb', subDir[i])
+            args.depthDir = os.path.join(datasetBase, subDir[i], 'depth')
+            args.maskDir = os.path.join(allBase, subDir[i], 'level0/canny')
+            args.camCalibFile = os.path.join(datasetBase, subDir[i], 'camera_calib_schenk.yml')
+            args.groundTruthFile = os.path.join(datasetBase, subDir[i], 'groundtruth_associated.txt')
+            args.outputDir = os.path.join(trainRgbBase, 'gt', subDir[i])
+            args.frameOffset = 5
+            args.lowerEdgeDistanceBoundary = 4
+            args.upperEdgeDistanceBoundary = args.lowerEdgeDistanceBoundary
+            args.projectionMode = 3
+            args.inpaintDepth = 2
+            args.minIsolatedPixelArea = 30
+            print(Utilities.argsToStr(args))
 
-        logging.info('Starting edge matching.')
-        edgeMatcher = EdgeMatcher(camera)
-        edgeMatcher.setFrameOffset(args.frameOffset)
-        edgeMatcher.setEdgeDistanceBoundaries(args.lowerEdgeDistanceBoundary, args.upperEdgeDistanceBoundary)
-        edgeMatcher.setMinIsolatedPixelArea(args.minIsolatedPixelArea)
+            # write configuration to output directory
+            f = open(os.path.join(args.outputDir, 'settings.txt'), 'w')
+            f.write(Utilities.argsToStr(args))
+            f.close()
 
-        frameFileNames = []
-        f = open(os.path.join(args.outputDir, 'records.txt'), 'w')
-        f.write('# timestamp d<=%d %d<d<=%d d>%d\n'%(args.lowerEdgeDistanceBoundary, args.lowerEdgeDistanceBoundary, args.upperEdgeDistanceBoundary, args.upperEdgeDistanceBoundary))
+            startTime = time.time()
+            logging.info('Loading data from camera calibration file.')
+            camera = Camera()
+            camera.loadFromFile(args.camCalibFile)
 
-        for a in gtHandler.data():
-            logging.info('Loading frame at timestamp %f (RGB: %s)' % (a.gt.timestamp, a.rgb))
+            logging.info('Loading data from associated ground truth file.')
+            gtHandler = TumGroundTruthHandler()
+            gtHandler.load(args.groundTruthFile)
 
-            # used for determining the correct filename, depending on the projection mode
-            frameFileNames.append(a.rgb)
+            if len(gtHandler.data()) < args.frameOffset:
+                raise ValueError('Number of input data is "%s" but must be greater than the frame offset "%d".' % (len(gtHandler.data()), args.frameOffset))
 
-            rgb = cv.imread(os.path.join(args.rgbDir, a.rgb), cv.IMREAD_UNCHANGED)
-            depth = cv.imread(os.path.join(args.depthDir, a.depth), cv.IMREAD_UNCHANGED)
-            mask = cv.imread(os.path.join(args.maskDir, a.rgb), cv.IMREAD_GRAYSCALE)
+            logging.info('Starting edge matching.')
+            edgeMatcher = EdgeMatcher(camera)
+            edgeMatcher.setFrameOffset(args.frameOffset)
+            edgeMatcher.setEdgeDistanceBoundaries(args.lowerEdgeDistanceBoundary, args.upperEdgeDistanceBoundary)
+            edgeMatcher.setMinIsolatedPixelArea(args.minIsolatedPixelArea)
 
-            if args.inpaintDepth == 1:
-                depth = ImageProcessing.reconstructDepthImg(depth, 5, cv.INPAINT_NS)
-            elif args.inpaintDepth == 2:
-                depth = ImageProcessing.reconstructDepthImg(depth, 5, cv.INPAINT_TELEA)
+            frameFileNames = []
+            f = open(os.path.join(args.outputDir, 'records.txt'), 'w')
+            f.write('# timestamp d<=%d %d<d<=%d d>%d\n' % (args.lowerEdgeDistanceBoundary, args.lowerEdgeDistanceBoundary, args.upperEdgeDistanceBoundary, args.upperEdgeDistanceBoundary))
 
-            frame = EdgeMatcherFrame()
-            frame.uid = a.gt.timestamp
-            frame.setRgb(rgb)
-            frame.setDepth(depth)
-            frame.setBoundaries(mask)
-            frame.setT(a.gt.q, a.gt.t)
+            for a in gtHandler.data():
+                if not os.path.exists(os.path.join(args.rgbDir, a.rgb)):
+                    logging.info('Skipping %s. File not found.' % (a.rgb))
+                    continue
 
-            meaningfulEdges, worseEdges = edgeMatcher.reprojectEdgesToConsecutiveFrameSet(frame, args.projectionMode, args.outputDir)
+                logging.info('Loading frame at timestamp %f (RGB: %s)' % (a.gt.timestamp, a.rgb))
 
-            if meaningfulEdges is None:
-                continue
-            
-            # determine correct filename
-            if args.projectionMode == EdgeMatcherMode.REPROJECT:
-                frameFileName = frameFileNames[0]
-            elif args.projectionMode == EdgeMatcherMode.BACKPROJECT or args.projectionMode == EdgeMatcherMode.CENTERPROJECT:
-                frameFileName = frameFileNames[args.frameOffset]
-            else:
-                raise ValueError('Unknown projection mode "%d".' % (args.projectionMode))
+                # used for determining the correct filename, depending on the projection mode
+                frameFileNames.append(a.rgb)
 
-            # save result
-            numBest = len(np.nonzero(meaningfulEdges))
-            numWorse = len(np.nonzero(worseEdges))
+                rgb = cv.imread(os.path.join(args.rgbDir, a.rgb), cv.IMREAD_UNCHANGED)
+                depth = cv.imread(os.path.join(args.depthDir, a.depth), cv.IMREAD_UNCHANGED)
+                #mask = cv.imread(os.path.join(args.maskDir, a.rgb), cv.IMREAD_GRAYSCALE)
 
-            f.write('%s %d %d %d\n'%(frameFileName, numBest, 0, numWorse))
-            f.flush()
-            cv.imwrite(os.path.join(args.outputDir, frameFileName), meaningfulEdges * 255)
-            logging.info('Saving "%s"' % (os.path.join(args.outputDir, frameFileName)))
-            frameFileNames.pop(0)
+                if args.inpaintDepth == 1:
+                    depth = ImageProcessing.reconstructDepthImg(depth, 5, cv.INPAINT_NS)
+                elif args.inpaintDepth == 2:
+                    depth = ImageProcessing.reconstructDepthImg(depth, 5, cv.INPAINT_TELEA)
 
-        elapsedTime = time.time() - startTime
-        print('\n')
-        logging.info('Finished in %.4f sec' % (elapsedTime))
-        f.close()
+                frame = EdgeMatcherFrame()
+                frame.uid = a.rgb  # a.gt.timestamp
+                frame.setRgb(rgb)
+                frame.setDepth(depth)
+                #frame.setBoundaries(mask)
+                frame.setT(a.gt.q, a.gt.t)
+
+                start = time.time()
+                resultFrame = edgeMatcher.reprojectEdgesToConsecutiveFrameSet(frame, args.projectionMode, args.outputDir)
+
+                if resultFrame is None:
+                    continue
+
+                logging.info('Meaningful edges processed in %f sec.' % (time.time() - start))
+                cleanedEdges = resultFrame.boundaries().copy()
+                cleanedEdges[np.nonzero(resultFrame.refinedMeaningfulEdges)] = 0
+
+                rgbCpy = resultFrame.rgb().copy()
+                rgbCpy[np.nonzero(cleanedEdges)] = [255, 0, 0]
+                rgbCpy[np.nonzero(resultFrame.refinedMeaningfulEdges)] = [0, 255, 0]
+
+                # fig = plt.figure(2)
+                # fig.suptitle('Meaningful Edges Results')
+                # fig.add_subplot(3, 1, 1)
+                # plt.axis('off')
+                # plt.title('Scaled Meaningful Edges')
+                # plt.imshow(resultFrame.scaledMeaningfulEdges, cmap='hot')
+
+                # fig.add_subplot(3, 1, 2)
+                # plt.axis('off')
+                # plt.title('Refined Meaningful Edges')
+                # plt.imshow(resultFrame.refinedMeaningfulEdges, cmap='hot')
+                
+                # fig.add_subplot(3, 1, 3)
+                # plt.axis('off')
+                # plt.title('Good/Bad Edges')
+                # plt.imshow(rgbCpy, cmap='hot')
+                # plt.show()
+
+                # save result
+                meaningfulEdges = resultFrame.refinedMeaningfulEdges
+
+                numBest = len(meaningfulEdges[meaningfulEdges > 0])
+                numWorse = len(cleanedEdges[cleanedEdges > 0])
+
+                frameFileName = resultFrame.uid
+                f.write('%s %d %d %d\n' % (frameFileName, numBest, 0, numWorse))
+                f.flush()
+                cv.imwrite(os.path.join(args.outputDir, frameFileName), meaningfulEdges * 255)
+                logging.info('Saving "%s"' % (os.path.join(args.outputDir, frameFileName)))
+                frameFileNames.pop(0)
+
+            elapsedTime = time.time() - startTime
+            print('\n')
+            logging.info('Finished in %.4f sec' % (elapsedTime))
+            f.close()
+
         sys.exit(0)
     except Exception as e:
         logging.error(e)
@@ -223,4 +274,10 @@ if __name__ == '__main__':
 python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/rgb' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/depth_inpaint_cv_talea' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/all/rgbd_dataset_freiburg2_xyz/level0/canny' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/em_output' -p 3 -f 4 -l 1 -u 1
 python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/rgb' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/depth' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/rgb/bdcn_40k_thinned' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/em_output' -p 3 -f 4 -l 1 -u 1 -id 2
 python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/rgb' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/depth' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/mask/canny' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_xyz/em_output_canny' -p 3 -f 4 -l 1 -u 1 -id 2
+
+python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/rgb' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/depth_inpaint_nans_m0' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/all/rgbd_dataset_freiburg2_xyz/level0/canny' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/em_output' -p 3 -f 4 -l 2 -u 2 -mipa 10
+python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/rgb' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/depth_inpaint_nans_m0' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/all/rgbd_dataset_freiburg2_xyz/level0/canny' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/rgbd_dataset_freiburg2_xyz/em_output' -p 3 -f 4 -l 2 -u 2 -mipa 10
+
+
+python reprojectEdgesFromConsecutiveFrameSet.py -g '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_plant/groundtruth_associated.txt' -r '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/mix/rgb/rgbd_dataset_freiburg1_plant/' -d '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_plant/depth' -m '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/all/rgbd_dataset_freiburg1_plant/level0/canny' -c '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/datasets/rgbd_dataset_freiburg1_plant/camera_calib_schenk.yml' -o '/run/user/1000/gvfs/smb-share:server=192.168.0.253,share=data/Master/train/mix/gt/rgbd_dataset_freiburg1_plant' -p 3 -f 3 -l 2 -u 2 -mipa 30
 '''
